998c44bd9dd0ec0c600923a58d8d3b7d82ff45e4
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-11-13 13:29:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+    int a=1, b=1, c;
+    cin >> n;
+    if(n==1) {
+        cout << a << endl;
+    }else if(n==2) {
+        cout << a << endl;
+        cout << b << endl;
+    }else{
+        cout << a <<endl;
+        cout << b <<endl;
+        for(int i=0;i<(n-2);i++){
+            c=a+b;
+            a=b;
+            b=c;
+            if (c<=n){
+                cout << c <<endl;
+            }
+        }
+    }
+    return 0;
 }
